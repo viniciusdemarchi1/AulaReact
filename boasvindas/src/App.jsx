19177e9575a785f60faaa39bf54917be626f10dc
viniciusdemarchi1 <viniciusaugustoferrari2@gmail.com>
2025-04-24 11:06:02 -0300

    24/04

---
+++ boasvindas/src/App.jsx
@@ -0,0 +1,28 @@
+import BoasVindas from "./Exemplo1"
+
+function app(){
+
+
+  return(
+
+<div>
+  <h1>Boas vindas ao React com JavaScript</h1>
+
+<p>
+  Este é o arquivo App.jsx que sera o nosso ponto de partida para
+  o desenvolvimento de aplicação React utilizando JavaScript
+</p>
+
+
+<BoasVindas nome="Vinícius"/>
+<BoasVindas nome="Danilo"/>
+<BoasVindas nome="Vk"/>
+</div>
+
+
+
+
+  )
+}
+
+export default app
---
+++ boasvindas/src/Exemplo1.jsx
@@ -0,0 +1,10 @@
+export default function BoasVindas({nome})
+{
+  return (
+    <div>
+       <p>
+        Olá {nome} seja bem vindo ao React.
+       </p>
+    </div>
+  );
+}
