5e51061b8593ff74f4e6e42ee6364824fb45d8e8
viniciusdemarchi1 <viniciusaugustoferrari2@gmail.com>
2025-10-30 08:55:54 -0300

    30-10-25

--- AulaBD/servidor/index.js
+++ AulaBD/servidor/index.js
@@ -72,3 +72,74 @@ app.get("/alunos/codigo", (req, res) => {
     });
 
 });
+
+app.post("/alunos", (req, res) => {
+    const { nome, cidade, estado } = req.body;
+
+    const sql = "insert into alunos(nome, cidade, estado) values (?,?,?)";
+
+    banco.query(sql, [nome, cidade, estado], (erro, result) => {
+
+        if (erro) {
+
+            console.log(erro);
+            return res.status(500).json({ error: "Erro ao cadastrar aluno" });
+
+        } else {
+            let mensagem = `Alunos ${nome} cadastrado com sucesso com o codigo ${res} `
+            console.log(mensagem);
+            return res.status(201).json({ message: mensagem });
+
+        }
+
+    });
+
+});
+
+
+app.put("/alunos/:id", (req, res) => {
+    const { id } = req.params;
+    const { nome, cidade, estado } = req.body;
+
+    const sql = "UPDATE alunos SET nome = ?, cidade = ?, estado = ? WHERE codigo = ?";
+
+    banco.query(sql, [nome, cidade, estado, id], (erro, result) => {
+
+        if (erro) {
+            console.log(erro);
+            return res.status(500).json({ error: "Erro ao atualizar aluno" });
+        }
+
+
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ message: "Aluno não encontrado" });
+        }
+
+        return res.status(200).json({ message: `Aluno com ID ${id} atualizado com sucesso` });
+
+    });
+
+
+});
+
+
+app.delete("/alunos/:id", (req, res) => {
+    const { id } = req.params;
+
+    const sql = "DELETE FROM alunos WHERE codigo = ? ";
+
+
+    banco.query(sql, [id], (erro, result) => {
+        if (erro) {
+            console.log(erro);
+            return res.status(500).json({ error: "Erro ao excluir o aluno" });
+        }
+
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ message: "Aluno não encontrado" });
+        }
+
+        return res.status(200).json({ message: `Aluno com ID ${id} atualizado com sucesso` });
+
+    });
+});
