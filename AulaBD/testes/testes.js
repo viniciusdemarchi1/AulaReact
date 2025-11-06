6f261ac19862c9b9ba2093a80d5217d373e77c3e
viniciusdemarchi1 <viniciusaugustoferrari2@gmail.com>
2025-11-06 09:18:30 -0300

    06/11/25

--- AulaBD/testes/testes.js
+++ AulaBD/testes/testes.js
@@ -1,19 +1,27 @@
-const { json } = require("body-parser");
+
 
 function consultar() {
-    fetch("http://localhost:3301/alunos")
+    fetch("http://localhost:3001/alunos")
         .then(res => res.json())
-    then(data => {
-        console.log("Alunos:", data);
-        document.getElementById("resultado").innerHTML = JSON.stringify(data, null, 2);
-    })
-        .cath(err => {
+        .then(data => {
+            console.log("Alunos:", data);
+            document.getElementById("resultado").innerHTML = JSON.stringify(data, null, 2);
+        })
+        .catch(err => {
             document.getElementById("resultado").innerHTML = "Erro ao obter dados da API !"
         });
 }
 
 function consultarPorCodigo(codigo) {
-
+ fetch(`http://localhost:3001/alunos/${codigo}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log("Alunos:", data);
+            document.getElementById("resultado").innerHTML = JSON.stringify(data, null, 2);
+        })
+        .catch(err => {
+            document.getElementById("resultado").innerHTML = "Erro ao obter dados da API !"
+        });
 }
 
 function cadastrar() {
